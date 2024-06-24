8285941621e8e8a69855de1988886ce86377082b
ClaudioRMalvino <claudiormal@gmail.com>
2024-06-24 16:39:25 -0400

    Updated code to increase readability.

--- spectral-method.py
+++ spectral-method.py
@@ -3,12 +3,6 @@ import matplotlib.pyplot as plt
 from scipy import constants
 from numpy.fft import rfft,irfft
 
-hbar = constants.hbar
-L = 10.0e-8
-N = 1000
-
-#dimension of the box
-grid  = np.linspace(0,L,N)
 
 def Psi(x):
     
@@ -19,17 +13,6 @@ def Psi(x):
     k = 5.0e+10
     
     return np.exp((-(x-x_0)**2)/2*(sigma**(2)))*np.exp(1j*k*x)
-  
-#creates an array of the complex type to hold the values of Psi(x)
-# then sets x = 0 and x = N equal to zero      
-psi = np.zeros(N,complex)
-psi[:] = Psi(grid)
-psi[0] = 0
-psi[N-1] = 0
-
-#holds the real and imaginary part of Psi(x)
-real = np.real(psi)
-imaginary = np.imag(psi)
 
 def dst(y):
     
@@ -45,9 +28,6 @@ def dst(y):
 
     return a
 
-#holds the coefficients alpha and eta
-alpha = dst(real)
-eta = dst(imaginary)
 
 def RPsi(k,n,t):
     """ This function defines the real part of the time dependent wave function."""
@@ -55,18 +35,6 @@ def RPsi(k,n,t):
     
     return (alpha[k]*np.cos(((np.pi**(2)*hbar*k**(2)) / (2*m*L**(2)))*t) 
             - eta[k]*np.sin(((np.pi**(2)*hbar*k**(2))/(2*m*L**(2)))*t))*np.sin((np.pi*k*n)/N)
-    
-RPsi_vals = []
-
-t = 1.0e-16
-k = np.arange(1,N)
-
-#Real part of the wave function is a sum from k =1 to N, normalized by 1/N at x_n
-# the loop below takes the sum of k = 1 to N for at every n = i from 0 to N
-for i in range(N):
-    RPsi_vals.append(1/N*np.sum(RPsi(k,i,t)))
-
-Real_Psi = np.array(RPsi_vals)
 
 def idst(a):
     
@@ -81,5 +49,40 @@ def idst(a):
 
     return y
 
+hbar = constants.hbar
+L = 10.0e-8
+N = 1000
+
+#dimension of the box
+grid  = np.linspace(0,L,N)
+
+#creates an array of the complex type to hold the values of Psi(x)
+# then sets x = 0 and x = N equal to zero      
+psi = np.zeros(N,complex)
+psi[:] = Psi(grid)
+psi[0] = 0
+psi[N-1] = 0
+
+#holds the real and imaginary part of Psi(x)
+real = np.real(psi)
+imaginary = np.imag(psi)
+
+#holds the coefficients alpha and eta
+alpha = dst(real)
+eta = dst(imaginary)
+    
+RPsi_vals = []
+
+t = 1.0e-16
+k = np.arange(1,N)
+
+#Real part of the wave function is a sum from k =1 to N, normalized by 1/N at x_n
+# the loop below takes the sum of k = 1 to N for at every n = i from 0 to N
+for i in range(N):
+    RPsi_vals.append(1/N*np.sum(RPsi(k,i,t)))
+
+Real_Psi = np.array(RPsi_vals)
+
 idst_Real_Psi = idst(Real_Psi)
 plt.plot(grid, idst_Real_Psi)
+plt.show()
